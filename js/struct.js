1796e98c86f9fe963843f0ea2ebf9dbd964686c2
Satyam Dengre <59261812+satyampgt4@users.noreply.github.com>
2021-01-18 23:24:45 +0530

    Update struct.js

--- js/struct.js
+++ js/struct.js
@@ -32,5 +32,26 @@ function advence(){
     
 }
 function back_restart(){
-    location.reload();
+    
+    document.getElementById("img0").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img1").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img2").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img3").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img4").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img5").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img6").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img7").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+    document.getElementById("img8").src = "https://satyampgt4.github.io/tictactoe/img/n.png";
+
+
+    document.getElementById("player").style.display= "flex";
+    document.getElementById("game-place").style.display = "none";
+    
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    n = 0;
+    w = 0;
+    level = 0;
+    player = 0;
+    mo = 1;
+    
 }
